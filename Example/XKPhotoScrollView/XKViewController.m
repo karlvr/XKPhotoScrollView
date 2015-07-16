55a73156383f9d7b90c79c74112a6cae3d9975de
Karl von Randow <karl@cactuslab.com>
2015-07-16 12:24:50 +1200

    Example app

--- Example/XKPhotoScrollView/XKViewController.m
+++ Example/XKPhotoScrollView/XKViewController.m
@@ -8,22 +8,75 @@
 
 #import "XKViewController.h"
 
-@interface XKViewController ()
+#import <XKPhotoScrollView/XKPhotoScrollView.h>
+
+@interface XKViewController () <XKPhotoScrollViewDataSource, XKPhotoScrollViewDelegate>
 
 @end
 
-@implementation XKViewController
+@implementation XKViewController {
+    NSArray *_views;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSMutableArray *views = [NSMutableArray array];
+    [views addObject:[self viewForImageNamed:@"photo1.jpg"]];
+    [views addObject:[self viewForImageNamed:@"photo2.jpg"]];
+    [views addObject:[self viewForImageNamed:@"photo3.jpg"]];
+    [views addObject:[self viewForImageNamed:@"photo4.jpg"]];
+    [views addObject:[self viewForImageNamed:@"photo5.jpg"]];
+    _views = [NSArray arrayWithArray:views];
+    
+    XKPhotoScrollView *photoScrollView = [XKPhotoScrollView new];
+    photoScrollView.dataSource = self;
+    
+    photoScrollView.frame = self.view.bounds;
+    photoScrollView.backgroundColor = [UIColor blackColor];
+    
+    [self.view addSubview:photoScrollView];
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark - Private
+
+- (UIView *)viewForImageNamed:(NSString *)imageName
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    UIImage *image = [UIImage imageNamed:imageName];
+    UIImageView *view = [[UIImageView alloc] initWithImage:image];
+    return view;
 }
+
+#pragma mark - XKPhotoScrollView
+
+#pragma mark XKPhotoScrollViewDataSource
+
+- (void)photoScrollView:(XKPhotoScrollView *)photoScrollView requestViewAtRow:(unsigned int)row col:(unsigned int)col
+{
+    UIView *view = _views[col];
+    NSLog(@"VIEW %@", view);
+    
+    [photoScrollView setView:view atRow:0 col:col placeholder:NO];
+}
+
+- (void)photoScrollView:(XKPhotoScrollView *)photoScrollView cancelRequestAtRow:(unsigned int)row col:(unsigned int)col
+{
+    
+}
+
+- (unsigned int)photoScrollViewRows:(XKPhotoScrollView *)photoScrollView
+{
+    return 1;
+}
+
+- (unsigned int)photoScrollViewCols:(XKPhotoScrollView *)photoScrollView
+{
+    return _views.count;
+}
+
+#pragma mark XKPhotoScrollViewDelegate
+
+
 
 @end
