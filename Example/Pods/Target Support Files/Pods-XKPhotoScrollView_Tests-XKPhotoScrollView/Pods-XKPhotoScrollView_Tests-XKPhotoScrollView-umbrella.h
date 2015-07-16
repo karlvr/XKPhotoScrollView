59cde9a96c4c2dcd6067d1473253e4fd917ffcfd
Karl von Randow <karl@xk72.com>
2015-07-17 06:32:36 +1200

    pod update

--- Example/Pods/Target Support Files/Pods-XKPhotoScrollView_Tests-XKPhotoScrollView/Pods-XKPhotoScrollView_Tests-XKPhotoScrollView-umbrella.h
+++ Example/Pods/Target Support Files/Pods-XKPhotoScrollView_Tests-XKPhotoScrollView/Pods-XKPhotoScrollView_Tests-XKPhotoScrollView-umbrella.h
@@ -1,6 +1,9 @@
 #import <UIKit/UIKit.h>
 
 #import "XKPhotoScrollView.h"
+#import "XKPhotoScrollViewDataSource.h"
+#import "XKPhotoScrollViewDelegate.h"
+#import "XKPhotoScrollViewViewState.h"
 
 FOUNDATION_EXPORT double XKPhotoScrollViewVersionNumber;
 FOUNDATION_EXPORT const unsigned char XKPhotoScrollViewVersionString[];
