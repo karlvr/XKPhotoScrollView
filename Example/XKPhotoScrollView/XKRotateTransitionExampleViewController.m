a8799f5f021ee2dbac5840cb142fc527d2de87af
Karl von Randow <karl@xk72.com>
2015-07-17 22:10:02 +1200

    Example: transition & rotate, go back to allowing rotation

--- Example/XKPhotoScrollView/XKRotateTransitionExampleViewController.m
+++ Example/XKPhotoScrollView/XKRotateTransitionExampleViewController.m
@@ -16,53 +16,25 @@
 
 @implementation XKRotateTransitionExampleViewController
 
-- (void)dealloc
+- (NSUInteger)supportedInterfaceOrientations
 {
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    /* We allow rotation, as we watch to see that rotation and then respond by presenting the photo scroll view */
+    return UIInterfaceOrientationMaskAll;
 }
 
-- (void)viewDidAppear:(BOOL)animated
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
 {
-    [super viewDidAppear:animated];
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
     
-    [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(orientationDidChangeNotification:) name:UIDeviceOrientationDidChangeNotification object:nil];
+    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+        if (size.width > size.height) {
+            [self goFullScreen];
+        } else {
+            [self dismissViewControllerAnimated:YES completion:NULL];
+        }
+    } completion:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+        
+    }];
 }
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-    [super viewWillDisappear:animated];
-    
-    [[UIDevice currentDevice] endGeneratingDeviceOrientationNotifications];
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIDeviceOrientationDidChangeNotification object:nil];
-}
-
-#pragma mark - Notifications
-
-- (void)orientationDidChangeNotification:(NSNotification *)notification
-{
-    const UIDeviceOrientation orientation = [UIDevice currentDevice].orientation;
-    
-    if (UIDeviceOrientationIsLandscape(orientation)) {
-        [self goFullScreen];
-    } else {
-        [self dismissViewControllerAnimated:YES completion:NULL];
-    }
-}
-
-//- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
-//{
-//    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
-//    
-//    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
-//        if (size.width > size.height) {
-//            [self goFullScreen];
-//        } else {
-//            [self dismissViewControllerAnimated:YES completion:NULL];
-//        }
-//    } completion:^(id<UIViewControllerTransitionCoordinatorContext> context) {
-//        
-//    }];
-//}
 
 @end
