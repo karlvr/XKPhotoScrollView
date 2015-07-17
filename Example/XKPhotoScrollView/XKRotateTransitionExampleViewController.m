aedffd11608cdd1397a604cce280f1e69f318d27
Karl von Randow <karl@cactuslab.com>
2015-07-17 15:49:12 +1200

    Example: rotation example now uses device orientation
    Instead of view rotation, so we don't get both orientation changes visible

--- Example/XKPhotoScrollView/XKRotateTransitionExampleViewController.m
+++ Example/XKPhotoScrollView/XKRotateTransitionExampleViewController.m
@@ -16,19 +16,53 @@
 
 @implementation XKRotateTransitionExampleViewController
 
-- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
+- (void)dealloc
 {
-    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
-    
-    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
-        if (size.width > size.height) {
-            [self goFullScreen];
-        } else {
-            [self dismissViewControllerAnimated:YES completion:NULL];
-        }
-    } completion:^(id<UIViewControllerTransitionCoordinatorContext> context) {
-        
-    }];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(orientationDidChangeNotification:) name:UIDeviceOrientationDidChangeNotification object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    [[UIDevice currentDevice] endGeneratingDeviceOrientationNotifications];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIDeviceOrientationDidChangeNotification object:nil];
+}
+
+#pragma mark - Notifications
+
+- (void)orientationDidChangeNotification:(NSNotification *)notification
+{
+    const UIDeviceOrientation orientation = [UIDevice currentDevice].orientation;
+    
+    if (UIDeviceOrientationIsLandscape(orientation)) {
+        [self goFullScreen];
+    } else {
+        [self dismissViewControllerAnimated:YES completion:NULL];
+    }
+}
+
+//- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
+//{
+//    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+//    
+//    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+//        if (size.width > size.height) {
+//            [self goFullScreen];
+//        } else {
+//            [self dismissViewControllerAnimated:YES completion:NULL];
+//        }
+//    } completion:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+//        
+//    }];
+//}
 
 @end
