dc299d1ce68220cd01c9a3073bf2139f47ab6ea7
Karl von Randow <karl@xk72.com>
2015-07-18 17:20:31 +1200

    Remove Pods dir

--- Example/Pods/Target Support Files/Pods-XKPhotoScrollView_Example-XKPhotoScrollView/Pods-XKPhotoScrollView_Example-XKPhotoScrollView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_XKPhotoScrollView_Example_XKPhotoScrollView : NSObject
-@end
-@implementation PodsDummy_Pods_XKPhotoScrollView_Example_XKPhotoScrollView
-@end
--- Example/Pods/Target Support Files/Pods-XKPhotoScrollView_Tests/Pods-XKPhotoScrollView_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_XKPhotoScrollView_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_XKPhotoScrollView_TestsVersionString[];
-
